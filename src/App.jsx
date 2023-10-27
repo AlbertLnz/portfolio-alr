9324678e70427fdbb2d580b9e70960c17a7f1562
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-27 12:27:32 +0200

    grid distribution of dashboard on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,41 @@ function App() {
   return (
     <div className="h-screen">
       <div className="grid lg:grid-rows-10 lg:grid-cols-10 h-full">
-        <div className="row-span-2 col-span-2 bg-red-300">
+        <div className="lg:row-span-2 lg:col-span-2 bg-red-300">
           <p>PHOTO</p>
         </div>
-        <div className="row-span-2 col-span-5 bg-blue-300">
+        <div className="lg:row-span-2 lg:col-span-5 bg-blue-300">
           <p>NAME</p>
         </div>
-        <div className="row-span-2 col-span-1 bg-green-300">
+        <div className="lg:row-span-2 lg:col-span-1 bg-green-300">
           <p>OPEN</p>
         </div>
-        <div className="row-span-2 col-span-1 bg-slate-300">
+        <div className="lg:row-span-2 lg:col-span-1 bg-slate-300">
           <p>EN</p>
         </div>
-        <div className="row-span-2 col-span-1 bg-orange-300">
+        <div className="lg:row-span-2 lg:col-span-1 bg-orange-300">
           <p>DARK</p>
         </div>
-        <div className="row-span-6 col-span-4 bg-slate-800">
+        <div className="lg:row-span-6 lg:col-span-4 bg-slate-800">
           <p>PROJECTS</p>
         </div>
-        <div className="row-span-5 col-span-3 bg-slate-200">
+        <div className="lg:row-span-5 lg:col-span-3 bg-slate-200">
           <p>CHALLENGE</p>
         </div>
-        <div className="row-span-5 col-span-3 bg-blue-800">
+        <div className="lg:row-span-5 lg:col-span-3 bg-blue-800">
           <p>TOOLS</p>
         </div>
 
-        <div className="row-span-3 col-span-4 bg-orange-800">
+        <div className="lg:row-span-3 lg:col-span-4 bg-orange-800">
           <p>STUDIES</p>
         </div>
-        <div className="row-span-3 col-span-1 bg-pink-400">
+        <div className="lg:row-span-3 lg:col-span-1 bg-pink-400">
           <p>LANGUAGES</p>
         </div>
-        <div className="row-span-3 col-span-1 bg-stone-400">
+        <div className="lg:row-span-3 lg:col-span-1 bg-stone-400">
           <p>FIGMA</p>
         </div>
-        <div className="row-span-2 col-span-4 bg-green-800">
+        <div className="lg:row-span-2 lg:col-span-4 bg-green-800">
           <p>COURSES</p>
         </div>
       </div>
