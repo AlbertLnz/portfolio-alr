ee92410bb31dd7e86e12752beef2d351267f4aa4
AlbertLnz <albert2000_lanza@hotmail.com>
2023-10-30 17:04:14 +0100

    TimeLine added to CoursesComponent + responsive

--- src/components/CoursesComponent.jsx
+++ src/components/CoursesComponent.jsx
@@ -3,20 +3,26 @@ import { useTranslation } from "react-i18next";
 import calendar_back from "../assets/images/CoursesComponent/calendar_back.svg";
 import calendar_now from "../assets/images/CoursesComponent/calendar_now.svg";
 import calendar_timeline from "../assets/images/CoursesComponent/calendar_timeline.png";
+import CoursesTimeLineComponent from "./CoursesTimeLineComponent";
 
 const CoursesComponent = () => {
   const [t] = useTranslation("global");
-  const numOfCourses = 3;
-  const [selectorCourse, setSelectorCourse] = useState(0);
+  const numOfCourses = 12;
+  const [selectorCourse, setSelectorCourse] = useState(1);
+  const [typeColor, setTypeColor] = useState("");
 
   const handleIncrementCourse = () => {
-    selectorCourse >= 0 && selectorCourse < numOfCourses - 1
+    selectorCourse >= 1 && selectorCourse < numOfCourses
       ? setSelectorCourse(selectorCourse + 1)
       : selectorCourse;
   };
 
   const handleDecrementCourse = () => {
-    selectorCourse > 0 ? setSelectorCourse(selectorCourse - 1) : selectorCourse;
+    selectorCourse > 1 ? setSelectorCourse(selectorCourse - 1) : selectorCourse;
+  };
+
+  const handleColorOfType = (color) => {
+    setTypeColor(color);
   };
 
   return (
@@ -25,49 +31,55 @@ const CoursesComponent = () => {
         Courses
       </h3>
       <div className="flex-grow grid grid-cols-10 rounded-lg bg-[#171719]">
-        <div className="col-span-2 flex flex-col place-items-center ml-3 my-2 rounded-lg">
+        <div className="col-span-2 flex flex-col place-items-center justify-center ml-3 my-2 rounded-lg">
           <button
-            className="flex items-center justify-center h-full w-1/2 rounded-t-xl border borde-white bg-[#010101] hover:bg-[#23272f]"
-            onClick={handleIncrementCourse}
-          >
-            <img src={calendar_now} alt="calendar_now" />
-          </button>
-          <button
-            className="flex items-center justify-center h-full w-1/2 rounded-b-xl border borde-white bg-[#010101] hover:bg-[#23272f]"
+            className="flex items-center justify-center py-2 px-2 hover:bg-[#23272f]"
             onClick={handleDecrementCourse}
           >
             <img src={calendar_back} alt="calendar_back" />
           </button>
+
+          <button
+            className="flex items-center justify-center py-2 px-2 hover:bg-[#23272f]"
+            onClick={handleIncrementCourse}
+          >
+            <img src={calendar_now} alt="calendar_now" />
+          </button>
         </div>
         <div className="col-span-8 grid grid-rows-3 mr-3 my-2 rounded-lg">
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">
+            <p className="text-md font-bold line-clamp-1">
               {t(`coursesSection.courses.${selectorCourse}.title`)}
             </p>
-            <p className="text-sm font-bold mr-2 px-4 py-0.5 rounded-full text-black bg-yellow-300">
+            <p
+              className={`text-sm font-bold mr-2 px-4 py-0.5 rounded-full text-black ${typeColor}`}
+            >
               {t(`coursesSection.courses.${selectorCourse}.type`)}
             </p>
           </div>
           <div className="flex items-center gap-6 ml-4">
             <a
               href={t(`coursesSection.courses.${selectorCourse}.certificate`)}
-              className="text-black text-sm rounded-full px-4 py.0.5 bg-slate-200"
+              className="text-black text-xs rounded-full px-3 py.0.5 bg-slate-200"
             >
               Certificate
             </a>
             <a
               href={t(`coursesSection.courses.${selectorCourse}.link_course`)}
-              className="text-black text-sm rounded-full px-4 py.0.5 bg-slate-200"
+              className="text-black text-xs rounded-full px-3 py.0.5 bg-slate-200"
             >
               Course
             </a>
-            <p className="text-black text-sm rounded-full px-4 py.0.5 bg-slate-200">
+            <p className="text-black text-xs rounded-full px-3 py.0.5 bg-slate-200">
               {t(`coursesSection.courses.${selectorCourse}.date`)}
             </p>
           </div>
           <div className="flex place-items-end">
             <img src={calendar_timeline} alt="" className="w-6" />
-            <p>{selectorCourse}</p>
+            <CoursesTimeLineComponent
+              position={selectorCourse}
+              color={handleColorOfType}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/CoursesTimeLineComponent.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+const CoursesTimeLineComponent = ({ position, color }) => {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const circles = [
+    { id: 1, active: currentStep === 1, color: "bg-green-300" },
+    { id: 2, active: currentStep === 2, color: "bg-yellow-200" },
+    { id: 3, active: currentStep === 3, color: "bg-green-300" },
+    { id: 4, active: currentStep === 4, color: "bg-yellow-200" },
+    { id: 5, active: currentStep === 5, color: "bg-blue-400" },
+    { id: 6, active: currentStep === 6, color: "bg-blue-400" },
+    { id: 7, active: currentStep === 7, color: "bg-yellow-200" },
+    { id: 8, active: currentStep === 8, color: "bg-yellow-200" },
+    { id: 9, active: currentStep === 9, color: "bg-yellow-200" },
+    { id: 10, active: currentStep === 10, color: "bg-blue-400" },
+    { id: 11, active: currentStep === 11, color: "bg-yellow-200" },
+    { id: 12, active: currentStep === 12, color: "bg-yellow-200" },
+  ];
+
+  useEffect(() => {
+    console.log(position);
+    setCurrentStep(position);
+  }, [position]);
+  color(circles[currentStep - 1].color);
+
+  return (
+    <div className="container flex flex-col items-center justify-center mx-2 w-full h-6">
+      <div className="steps flex w-full items-center justify-between relative">
+        {circles.map((circle) => (
+          <span
+            key={circle.id}
+            className={`z-10 rounded-full w-4 h-4 text-[10px] flex justify-center text-black ${
+              circle.color
+            } ${circle.active ? "active font-bold" : ""}`}
+          >
+            {circle.id}
+          </span>
+        ))}
+
+        <div className="flex items-center absolute h-4 w-full z-0">
+          <span
+            className="absolute h-1/2 bg-[#2b3037] rounded-full px-1 transition-all duration-500"
+            style={{
+              width: `${((currentStep - 1) / (circles.length - 1)) * 100}%`,
+            }}
+          ></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoursesTimeLineComponent;
